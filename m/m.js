68de2f5eb4247d186598ce533f915fda6d99dbca
Zheard <32337604+Zheard@users.noreply.github.com>
2024-05-07 08:58:11 -0500

    Update m.js

--- m/m.js
+++ m/m.js
@@ -1,8 +1,8 @@
-Hello!
-This is a privacy code. If you have the key you can access to data base and manage all content of this blog.
-Blog the Zheard is a Website with language HTML and codes with IA not show in this year. Probably the key is a word very common in 2062.
-----
-If you have the key, please write it in the browser at the end of the url.
+// Hello.
+// This is a privacy code. If you have the key you can access to data base and manage all content of this blog.
+// Blog the Zheard is a Website with language HTML and codes with IA not show in this year. Probably the key is a word very common in 2062.
+// ----
+// If you have the key, please write it in the browser at the end of the url.
 
-Atte.
-DraehZ.
+// Atte.
+// DraehZ.
